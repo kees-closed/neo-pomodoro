097725e45c6517e850dab3b8a85a54c9e8077823
K. de Jong <kjong+dev@neobits.nl>
2020-04-28 17:33:13 +0200

    Added interactive prompt

--- pomodoro.sh
+++ pomodoro.sh
@@ -8,15 +8,15 @@ command_notify_send="/usr/bin/notify-send"
 icon_break_over="face-glasses"
 icon_long_break="face-cool"
 icon_short_break="face-tired"
-prompt=false
+interactive=false
 wait_minute=true
 
 
 usage() {
-  printf "  This program helps managing time using the Pomodoro technique\n"
-  printf "    -b, --breaks      Sets the amount of short breaks before a long break\n"
-  printf "    -p, --prompt      Enables keyboard interaction to initiate each break\n"
-  printf "    -t, --break-time  Sets the time for the long breaks\n"
+  printf "This program helps managing time using the Pomodoro technique\n"
+  printf "  -b, --breaks      Sets the amount of short breaks before a long break\n"
+  printf "  -i, --interactive Enables interactive options before each break\n"
+  printf "  -t, --break-time  Sets the time in minutes for the long breaks\n"
 }
 
 
@@ -24,7 +24,7 @@ desktop_notification() {
   local icon="$1"
   local message="$2"
 
-  if [[ "$prompt" == true && "$icon" != "$icon_break_over" ]]; then
+  if [[ "$interactive" == true && "$icon" != "$icon_break_over" ]]; then
     message+=" (check prompt)"
   fi
 
@@ -39,10 +39,39 @@ desktop_notification() {
 }
 
 
-break_prompt() {
-  if "$prompt"; then
-    read -p "--:-- Press enter to start break"
-    printf "%s Break started\n" "$(date +%H:%M)"
+interactive_prompt() {
+  local long_break="$1"
+  local option
+
+  if "$interactive"; then
+    printf -- "--:-- Press enter to start break
+--:-- Press l|L followed by enter to start long break
+--:-- Press i|I followed by enter to stop interactive mode\n"
+
+    while (( "${#option[@]}" != 1 || "${#option}" != 1 )); do
+      read -rp "--:-- Choose one option: " option
+      if [[ "$option" == "" ]]; then
+        break
+      fi
+    done
+
+    case "$option" in
+      i | I)
+        interactive=false
+        printf "%s Break started, interactive mode disabled\n" "$(date +%H:%M)"
+        ;;
+      l | L)
+        if [[ "$long_break" != true ]]; then
+          break_counter=0
+          printf "%s Starting forced long break of %s minutes\n" "$(date +%H:%M)" "$(( break_time / 60 ))"
+          return 3
+        fi
+        printf "%s Starting long break of %s minutes (already scheduled)\n" "$(date +%H:%M)" "$(( break_time / 60 ))"
+        ;;
+      * )
+        printf "%s Starting break\n" "$(date +%H:%M)"
+        ;;
+    esac
   fi
 }
 
@@ -59,19 +88,23 @@ pomodoro_timer() {
       local breaks_taken=$(( breaks - break_counter ))
 
       if (( breaks_taken < 0 )); then
+        wait_minute=false
+        break_counter=0
         printf "%s Time to have a long break of %s minutes\n" "$(date +%H:%M)" "$(( break_time / 60 ))"
         desktop_notification "$icon_long_break" "Time to have a long break of $(( break_time / 60 )) minutes"
-        break_prompt
+        interactive_prompt true
         sleep "$break_time"
-        break_counter=0
-        wait_minute=false
 
       elif (( breaks_taken >= 0 )); then
+        wait_minute=false
         printf "%s Time to have a short break of 5 minutes %s\n" "$(date +%H:%M)" "($break_counter/$breaks)"
         desktop_notification "$icon_short_break" "Time to have a short break of 5 minutes ($break_counter/$breaks)"
-        break_prompt
+        interactive_prompt
+        if (( $? == 3 )); then
+          sleep "$break_time"
+          continue
+        fi
         sleep 300
-        wait_minute=false
       fi
 
       desktop_notification "$icon_break_over" "Time to work for $timer minutes"
@@ -92,16 +125,22 @@ while (( $# > 0 )); do
     -b | --breaks)
       shift
       breaks="$1"
+      if (( breaks < 1 )); then
+        printf "Error: The amount of short breaks cannot be less than 1\n\n"
+        usage
+        exit 1
+      fi
       ;;
-    -p | --prompt)
+    -i | --interactive)
       shift
-      prompt=true
+      interactive=true
       ;;
     -t | --break-time)
       shift
       break_time=$(( $1 * 60 ))
       if (( break_time < 600 )); then
-        printf "Long breaks cannot be shorter than 10 minutes\n"
+        printf "Error: Long breaks cannot be shorter than 10 minutes\n\n"
+        usage
         exit 1
       fi
       ;;
